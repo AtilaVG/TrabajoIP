20b1d9fa861998f6c36fad63af0095072b35a0c5
Ana Cristina Acosta Hernández <55719681+AnaAcostaH@users.noreply.github.com>
2024-01-03 12:55:46 +0100

    Unificado todo a dos unidades

--- ugaraje.pas
+++ ugaraje.pas
@@ -14,6 +14,7 @@ CONST
   PlazaFin= 20;
 
 TYPE
+  //RELATIVO A GARAJE
   tGaraje =  ARRAY [PisoIni..PisoFin,PlazaIni..PlazaFin] of tPlaza;
   tFichero = FILE OF tGaraje;
 
@@ -24,6 +25,32 @@ TYPE
   PROCEDURE Distintivo(VAR coche: tCoche, VAR distintivo: string);
 
 
+  //RELATIVOS A PLAZA
+  tPlaza = RECORD
+    tamanio: boolean; //1 grande y 0 pequenia
+    ocupado: boolean;
+    coche: tCoche;
+  END;
+
+  tGanancias = RECORD
+      gananciaTotal: real;
+  END;
+
+  PROCEDURE PonerPlazaVacia(VAR plaza:tPlaza; aux: integer; aux1: integer);
+  PROCEDURE Aparcar(VAR plaza:tPlaza; VAR coche: tCoche);
+  PROCEDURE Salir(VAR plaza:tPlaza; VAR matricula: String; aux: integer; aux1: integer; VAR tGanancias: tGanancias);
+
+
+  //RELATIVO A COCHE
+  intervalo = 1..4;
+  tCoche = RECORD
+    numMatricula: String[7];
+    distintivo: string[20];
+
+  END;
+
+  PROCEDURE AsignarCoche(VAR coche1: tCoche; VAR coche2: tCoche);
+  PROCEDURE CrearCoche(VAR coche: tCoche; VAR numMat: string;VAR distintivo: string);
 
 
 implementation
@@ -33,6 +60,7 @@ PROCEDURE CrearGarajeVacio(VAR garaje: tGaraje);
 VAR
   aux,aux1: integer;
 BEGIN
+
   for aux:= 1 to 4 do
       begin
       for aux1:= 1 to 20 do
@@ -104,8 +132,7 @@ PROCEDURE EntradaVehiculo(VAR fichero: tFichero; VAR garaje: tGaraje; VAR tamani
 VAR
   tam: String;
 BEGIN
-  writeln('Escribe si el coche es grande o pequenio');
-  readln(tam);
+
   HayPlazas(garaje, tamanio);
 
 END;
@@ -186,6 +213,99 @@ begin
 
 end;
 
+// EMPEZAMOS PROCEDURES DE PLAZA
+
+PROCEDURE PonerPlazaVacia(VAR plaza:tPlaza; aux: integer; aux1: integer);
+BEGIN
+  plaza.ocupado:= false;
+
+  if (((aux-1)*20+aux1) mod 6 = 0) then
+     begin
+       plaza.tamanio:= true;
+     end
+  else
+      plaza.tamanio:= false;
+END;
+
+PROCEDURE Aparcar(VAR plaza:tPlaza; VAR coche: tCoche; VAR tamanio: string);
+VAR
+  aux, aux1: integer;
+BEGIN
+  IF tamanio =  THEN BEGIN
+
+  plaza.ocupado:= true;
+  AsignarCoche(plaza.coche,coche);
+END;
+
+end;
+
+PROCEDURE Salir(VAR plaza:tPlaza; VAR matricula: String; aux: integer; aux1: integer; VAR tGanancias: tGanancias); //ojo con si es grande o pequenia que es diferente precio
+VAR
+  opcion: String;
+  minutos, precioEstacionar: real;
+BEGIN
+  precioEstacionar := 0;
+
+  for aux:= 1 to 4 do
+      begin
+      for aux1:= 1 to 20 do
+          begin
+               if (plaza.coche.numMatricula = matricula)then
+                  plaza.ocupado := false;
+
+          end;
+      end;
+  writeln('Escribe si tu coche es grande o pequenio');
+  readln(opcion);
+  writeln('Escribe el tiempo que has estado en minutos');
+  readln(minutos);
+
+  IF (opcion = 'grande') THEN BEGIN
+     IF (minutos > 60) THEN
+        precioEstacionar := (0.02 * minutos)
+     ELSE
+        precioEstacionar := (0.06 * minutos);
+  END
+
+  ELSE //la opcion es 'pequenia'
+  IF (minutos > 60) THEN
+     precioEstacionar := 0.01 * minutos
+  ELSE
+      precioEstacionar := 0.04 * minutos;
+
+  tGanancias.gananciaTotal := tGanancias.gananciaTotal + precioEstacionar;
+
+END;
+
+//EMPEZAMOS CON COCHE
+
+
+PROCEDURE AsignarCoche(VAR coche1: tCoche; VAR coche2: tCoche); //en vez de borrar los datos del coche antiguo, se sobreescriben con el nuevo
+BEGIN
+  coche1.numMatricula:= coche2.numMatricula;
+  coche1.distintivo:= coche2.distintivo;
+END;
+
+
+PROCEDURE CrearCoche(VAR coche: tCoche; VAR numMat: string;VAR distintivo: string);
+
+BEGIN
+  coche.numMatricula:= numMat;
+  coche.distintivo:= distintivo;
+END;
+
+PROCEDURE SacarCoche(VAR coche: tCoche);
+VAR
+  minutos: integer;
+
+BEGIN
+  writeln('Escribe cuántos minutos has tenido el coche aparcado');
+  readln(minutos);
+
+
+
+
+END;
 
 end.
 
